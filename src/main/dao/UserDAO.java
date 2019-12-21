09d2d2a26117a647150095853ae693ef69133d62
Alexander-Bolotov <37707228+Alexander-Bolotov@users.noreply.github.com>
2019-12-21 11:39:54 +0400

    commit

---
+++ src/main/dao/UserDAO.java
@@ -0,0 +1,118 @@
+package src.main.dao;
+
+import src.main.model.User;
+
+import javax.tools.StandardJavaFileManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+    private Connection connection;
+
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<User> getAllUser() {
+        List<User> userList = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                userList.add(new User(resultSet.getLong("id"), resultSet.getString("name"), resultSet.getString("password")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
+    }
+
+    public User getUserByName(String name) {
+        User userResult = new User();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("select * from users where name= ?")) {
+            preparedStatement.setString(1, name);
+            ResultSet result = preparedStatement.executeQuery();
+            result.next();
+            userResult.setId(result.getLong("id"));
+            userResult.setName(result.getString("name"));
+            userResult.setPassword(result.getString("password"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userResult;
+    }
+
+    public boolean nameIsExist(String name) {
+        return getAllUser()
+                .stream()
+                .map(User::getName)
+                .anyMatch(x -> x.equals(name));
+    }
+
+    public void addUser(User user) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (name, password) VALUES (?, ?)")) {
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public void deleteUser(User user) {
+//        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE name=?")) {
+//            preparedStatement.setString(1, user.getName());
+//            preparedStatement.executeUpdate();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    public void deleteUserByName(String name) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE name= ?")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.execute();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void editeUser(long id, String name, String password) {
+        User user = getUserById(id);
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET name=?  where id LIKE ?")) {
+            preparedStatement.setString(1, name);
+//            preparedStatement.setString(2, password);
+            preparedStatement.setLong(2, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User getUserById(Long id) {
+        User user = new User();
+        try (PreparedStatement preparedStatement = connection.prepareStatement("select * from users where id= ?")) {
+            preparedStatement.setLong(1, id);
+            ResultSet result = preparedStatement.executeQuery();
+            result.next();
+            user.setId(result.getLong("id"));
+            user.setName(result.getString("name"));
+            user.setPassword(result.getString("password"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+//    public boolean idIsExist(long id){
+//        try (Statement statement = connection.prepareStatement("SELECT COUNT * FROM table users")){
+//            long count = statement.execute();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
+
---
+++ src/main/servlet/EditeServlet.java
@@ -0,0 +1,34 @@
+package src.main.servlet;
+
+import src.main.model.User;
+import src.main.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/edie")
+public class EditeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserService userService = UserService.getInstance();
+        Long id = Long.parseLong(req.getParameter("id"));
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        User user = new User(id, name, password);
+
+        if (name == null || password == null || id == null) {
+            resp.getWriter().println("Не введен пароль или логин или ID");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            if (userService.editeUser(user)){
+                req.getRequestDispatcher("index.jsp").forward(req, resp);
+            } else {
+                resp.getWriter().println("Client not add");
+            }
+        }
+    }
+}
